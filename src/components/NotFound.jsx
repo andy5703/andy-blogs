295e44c622c03421a71f2eda5474defcefbaad9b
Andi <andianechitei@gmail.com>
2025-02-11 16:07:10 +0200

    redeploy

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -4,7 +4,7 @@ const NotFound = () => {
     return(
         <div className='not-found'>
             <h2 className="title">404 Not Found</h2>
-            <p>That page cannot be found!</p>#
+            <p>That page cannot be found!</p>
             <Link to='/'>Back to Homepage...</Link>
         </div>
     );
